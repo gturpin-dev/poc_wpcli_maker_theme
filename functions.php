d5393ed45ee48eba47054e54c7ebb732a0f34859
Guillaume <guillaume.turpin45@gmail.com>
2023-11-16 12:33:43 +0100

    :rotating_light: Fix Pint on functions.php

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ use Whodunit\Framework\Services\ConfigLoader;
 
 // Load autoloader
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-	require_once __DIR__ . '/vendor/autoload.php';
+    require_once __DIR__ . '/vendor/autoload.php';
 }
 
 // load all config files
